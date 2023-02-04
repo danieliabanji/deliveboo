12ff54c74bbec9e9a642b2005e3930c5e5237040
BiagioButta <biagiob940@gmail.com>
2023-02-04 17:21:14 +0100

    add button index

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -4,38 +4,36 @@
 
 <div class="container">
 
-   <div class="row my-5">
+  <div class="row my-5">
 
-        @foreach ($products as $product)
+    @foreach ($products as $product)
 
-        <div class="col-4">
-            <img src="{{$product->image}}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">{{$product->name}}</h5>
-              <p class="card-text">{{$product->description}}</p>
-              <h5>{{$product->price}}</h5>
+      <div class="col-4">
+        <img src="{{$product->image}}" class="card-img-top" alt="..." style="height: 300px">
+        <div class="card-body">
+          <h5 class="card-title">{{$product->name}}</h5>
+          <p class="card-text">{{$product->description}}</p>
+          <h5>{{$product->price}} &euro;</h5>
+          @if ($product->available == 1)
+            <span class="text-success">Disponibile</span>
+          @else
+              <span class="text-danger">Non disponibile</span>
+          @endif
+          @if ($product->discount)
+              <div>Lo sconto è del {{$product->discount}} %</div>
+          @endif
+          <div>
 
-              @if ($product->available == 1)
-              <span>Disponibile</span>
+            <a href="#" class="btn btn-primary">Mostra prodotto</a>
+            <a href="#" class="btn btn-success">Modifica</a>
+            <a href="#" class="btn btn-danger">Elimina</a>
 
-            @else
-                <span>Non disponibile</span>
-            @endif
-
-            @if ($product->discount)
-
-                <div>Lo sconto è {{$product->discount}} %</div>
-
-            @endif
-
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
           </div>
+        </div>
+      </div>
 
-
-
-        @endforeach
-   </div>
+    @endforeach
+  </div>
 </div>
 
 @endsection
