c5379a6a20b11df0e98551543ae9e29d72927465
StefaniaSperani <thedarkcry@hotmail.it>
2023-02-06 10:21:40 +0100

    add preview image js function

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,18 @@
-import './bootstrap';
-import '~resources/scss/app.scss';
-import * as bootstrap from 'bootstrap';
-import.meta.glob([
-    '../img/**'
-])
+import "./bootstrap";
+import "~resources/scss/app.scss";
+import * as bootstrap from "bootstrap";
+import.meta.glob(["../img/**"]);
+
+// // funzione di preview dell'immagine
+// const previewImage = document.getElementById('image');
+// previewImage.addEventListener('change', (event) =>{
+//     var oFReader = new FileReader();
+//     // var image  =  previewImage.files[0];
+//     // console.log(image);
+//     oFReader.readAsDataURL(previewImage.files[0]);
+
+//     oFReader.onload = function (oFREvent) {
+//         //console.log(oFREvent);
+//         document.getElementById("uploadPreview").src = oFREvent.target.result;
+//     };
+// });
