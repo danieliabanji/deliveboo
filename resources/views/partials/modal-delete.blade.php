dcfb9df068cac4ec47f61318a2d0c21b73e299ce
StefaniaSperani <thedarkcry@hotmail.it>
2023-02-09 12:42:01 +0100

    fix dashboard

--- resources/views/partials/modal-delete.blade.php
+++ resources/views/partials/modal-delete.blade.php
@@ -1,7 +1,7 @@
 <!-- Modal -->
 <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
+      <div class="modal-content border border-danger">
         <div class="modal-header">
           <h1 class="modal-title text-uppercase fs-5 text-danger fw-bold" id="deleteModalLabel">attenzione</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -10,8 +10,8 @@
             <p> Confermi di voler eliminare: <span id="modal-item-title" class="fw-bold text-my-dark"></span>?</p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="my-btn rounded-pill" data-bs-dismiss="modal">Chiudi</button>
-          <button type="button" class="btn btn-danger">Cancella</button>
+          <button type="button" class="btn mybtn-orange rounded-pill" data-bs-dismiss="modal">Chiudi</button>
+          <button type="button" class="btn btn-danger rounded-pill">Cancella</button>
         </div>
       </div>
     </div>
